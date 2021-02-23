8dc9c1a67971013ce99155a2804ac35f6ad5987d
amit942rawat <rawatamit942@gmail.com>
2021-02-23 11:23:19 +0530

    Commit by amit rawat

--- suntec-frontend-dev/src/app/app.component.ts
+++ suntec-frontend-dev/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'suntec-frontend-dev';
+  title = 'suntec-frontend-dev-AmitRawat';
 }
