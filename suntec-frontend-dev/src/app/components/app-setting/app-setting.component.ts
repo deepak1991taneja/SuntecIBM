1693b30231bb3af9a2469740491eab7cbf0cb5e0
MandeepSaini1016 <sainimandeep323@gmail.com>
2021-02-26 12:35:02 +0530

    base theme

--- suntec-frontend-dev/src/app/components/app-setting/app-setting.component.ts
+++ suntec-frontend-dev/src/app/components/app-setting/app-setting.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-app-setting',
   templateUrl: './app-setting.component.html',
-  styleUrls: ['./app-setting.component.scss']
+  styleUrls: ['./app-setting.component.scss'],
+  animations: [
+    trigger('indicatorRotate', [
+      state('collapsed', style({transform: 'rotate(0deg)'})),
+      state('expanded', style({transform: 'rotate(180deg)'})),
+      transition('expanded <=> collapsed',
+        animate('225ms cubic-bezier(0.4,0.0,0.2,1)')
+      ),
+    ])
+  ]
 })
 export class AppSettingComponent implements OnInit {
-
   options: FormGroup;
-
   constructor(fb: FormBuilder) {
     this.options = fb.group({
       bottom: 0,
